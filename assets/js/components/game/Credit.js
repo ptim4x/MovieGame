5b846e62d815203c8b2b4c7a2ec51bb40ca8b511
ptim4x <ptimax@gmail.com>
2022-12-06 22:37:50 +0100

    fun(front): make de last question

--- assets/js/components/game/Credit.js
+++ assets/js/components/game/Credit.js
@@ -1,20 +1,36 @@
 import React from "react";
+import {
+  FaThumbsUp,
+  FaQuestion,
+  FaGithub,
+  FaStar,
+  FaBeer,
+} from "react-icons/fa";
+import { HiCursorClick, HiEmojiHappy } from "react-icons/hi";
 
 /**
  * Credit component
  */
 const Credit = () => (
-  <div>
+  <div className="my-5">
     <h3>Merci d'avoir jouer.</h3>
-    <p>Source disponible sur github</p>
-    <div className="alert alert-info">
-      <a
-        href="https://github.com/ptim4x/MovieGame"
-        target="_blank"
-        rel="noreferrer"
-      >
-        https://github.com/ptim4x/MovieGame
-      </a>
+    <div className="alert alert-info mt-5">
+      <div className="h4 my-3">
+        <FaThumbsUp />
+        <FaQuestion />
+        <HiCursorClick />
+        <FaStar />
+        <a
+          href="https://github.com/ptim4x/MovieGame"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <span className="h3">:</span>
+        <FaBeer />
+        <HiEmojiHappy />
+      </div>
     </div>
   </div>
 );
